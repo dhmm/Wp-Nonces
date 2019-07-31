ae3be1f557e1654aa73f72c5c426224b2ee68176
unknown <dhmm@dhmm>
2019-07-31 12:21:16 +0300

    test nonce fiedl added and code standars fixes in tests

--- tests/WPNonceTest.php
+++ tests/WPNonceTest.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+namespace Moutlou;
+
 use PHPUnit\Framework\TestCase;
 use Moutlou\WPNonce;
 
@@ -12,18 +14,29 @@ class WPNonceTest extends TestCase
         $result = $wpNonce->createNonce();
         $this->assertEquals(1, $result);
     }
-
     public function testVerifyNonce()
     {
         $wpNonce = new WPNonce();
-        $result = $wpNonce->verifyNonce($nonce='test-nonce');
+        $result = $wpNonce->verifyNonce($nonce = 'test-nonce');
+        $this->assertEquals(1, $result);
+    }
+    public function testNonceField()
+    {
+        $wpNonce = new WPNonce();
+        $result = $wpNonce->nonceField();
         $this->assertEquals(1, $result);
     }
 }
 
-function wp_create_nonce() {
-  return 1;
+function wp_create_nonce()
+{
+    return 1;
 }
-function wp_create_verify_nonce($nonce) {
-  return 1;
+function wp_create_verify_nonce($nonce)
+{
+    return 1;
+}
+function wp_nonce_field()
+{
+    return 1;
 }
